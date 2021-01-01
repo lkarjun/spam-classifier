d25e71987daeb77e33f319ccd8e8e0b0a66269d9
lkarjun <lk.arjun@hotmail.com>
2021-01-01 22:07:50 +0530

    Updates: detect me.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 import uvicorn
+from input_processing import model_answer_please
 
 templates = Jinja2Templates(directory='templates')
 
@@ -9,3 +10,10 @@ app = FastAPI()
 @app.get('/')
 async def serve_home(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})
+
+@app.get('/detect-me')
+async def detect_me(message: str):
+    return {"answer is": model_answer_please(message)}
+
+if __name__ == "__main__":
+    uvicorn.run(app)
